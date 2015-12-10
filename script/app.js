9e0bbc13bd610a0838fa4e872e64a0edd07d96da
babakkhosravifar <babak.khosravifar@gmail.com>
2015-12-10 19:02:49 +0000

    Exercise-1

--- script/app.js
+++ script/app.js
@@ -1,1 +1,25 @@
 // Write your code here!
+var announcements = $("div.announcement");
+
+announcements.each(function() {
+
+    var x = $("<button>close</button>");
+    var currentDiv = $(this);
+
+    $(this).append(x);
+
+    x.on("click", function() {
+
+        var $button = $(this);
+
+        if ($button.text() === "close") {
+
+            currentDiv.find("p").hide();
+            $button.text("Show announcement");
+        }
+        else {
+            currentDiv.find("p").show();
+            $button.text("close");
+        }
+    });
+});
