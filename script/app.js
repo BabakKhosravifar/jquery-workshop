809b1ec5ac3883ddfd99cbd730ef6ccfff85db9b
babakkhosravifar <babak.khosravifar@gmail.com>
2015-12-10 20:56:50 +0000

    Exercise-2

--- script/app.js
+++ script/app.js
@@ -1,1 +1,25 @@
 // Write your code here!
+var imgSelect = $("img");
+
+ $("div.overlay").on("click", function(){
+            
+            $(this).css('display','none');
+          
+        });
+
+imgSelect.each(function(){
+    
+    var $img = $(this);
+    
+    $img.on("click", function(){
+        
+        var $url = $(this).attr("src");
+        var $alt = $(this).attr("alt");
+        
+        $("div.overlay").css('display','inline-flex');
+     //   $("div.overlay-img").append('<img src="'+$url+'"><p>'+$alt+'</p>');
+        $("p.overlay-text").text($alt);
+        $("img.overlay-img").attr("src", $url);
+       
+    });
+});
